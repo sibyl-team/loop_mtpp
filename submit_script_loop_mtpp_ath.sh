aa8541c774e19e10a5c4f92b41af3cc726cfc577
anna-pa-m <anna.muntoni@polito.it>
2022-03-30 13:44:41 +0000

    updt script

--- submit_script_loop_mtpp_ath.sh
+++ submit_script_loop_mtpp_ath.sh
@@ -2,56 +2,22 @@
 
 PRINTDIR="/home/amuntoni/sibyl-team/loop_mtpp/"
 beta=0.05
-ti=14
-fn_rate=$1
-adp_frac=$2
-tau=$3
-prob_th=0.00
-adapt_th="True"
+ti=$1
+fn_rate=$2
+adp_frac=$3
+tau=$4
+ath=True
 
 for alg in bp_gamma_ath
 do
-	for seed in $(seq 1 1 10) 
-	#for seed in 2
-	do
-		#for nobs in 60 90 120 150
-		for nobs in 300
+	for seed in $(seq 1 1 11)
+	do	
+		for nobs in 400
 		do
 			script=$(echo "script_loop_mtpp_"$alg".py")
-			flag=$seed"_"$alg"_"$nobs"_"$ti"_fnr_"$fn_rate"_af_"$adp_frac"_ath_"$adapt_th
-			#flag=$seed"_"$alg"_"$nobs"_fnr_"$fn_rate"_af_"$adp_frac
-			echo "#!/bin/bash" > slurm_$flag".sh"
-			echo " " >> slurm_$flag".sh"
-			echo "#SBATCH --job-name="$flag >> slurm_$flag".sh"
-			echo "#SBATCH --partition=global" >> slurm_$flag".sh"
-			echo "#SBATCH --time=240:00:00" >> slurm_$flag".sh"
-			if [ "$alg" == "bp_exp" ] || [ "$alg" == "bp_gamma" ]; then
-				echo "#SBATCH --mem=35GB" >> slurm_$flag".sh"
-			else
-				echo "#SBATCH --mem=35GB" >> slurm_$flag".sh"
-			fi
-			echo "#SBATCH --nodes=1" >> slurm_$flag".sh"
-			if [ "$alg" == "bp_exp" ] || [ "$alg" == "bp_gamma" ]; then
-				echo "#SBATCH --ntasks-per-node=32" >> slurm_$flag".sh"
-			else
-				echo "#SBATCH --ntasks-per-node=32" >> slurm_$flag".sh"
-			fi
-			echo "#SBATCH --output " $PRINTDIR"log_"$flag".out" >> slurm_$flag".sh"
-			echo "#SBATCH -e " $PRINTDIR"log_"$flag".err" >> slurm_$flag".sh"
-
-			echo submitting job $flag 
-			echo "module load gsl/2.5" >> slurm_$flag".sh"
-			echo "module load intel/python/3/2019.4.088/" >> slurm_$flag".sh"
-			echo "cd /home/amuntoni/sibyl-team/loop_mtpp/" >> slurm_$flag".sh" 
-			#start_time=$(date +%s)
-			echo "python3 " $script "--fn_rate" $fn_rate "--tau" $tau "-m" $seed "--adp_th" $adapt_th "-n" $nobs "-b " $beta " -i " $ti " --adp_frac " $adp_frac >> slurm_$flag".sh"	
-			#echo "python3 " $script "--fn_rate" $fn_rate "-m" $seed  "-n" $nobs "-b " $beta " -i " $ti " --adp_frac " $adp_frac >> slurm_$flag".sh"	
-
-			sbatch slurm_$flag".sh"
-			mv slurm_$flag".sh" "slurm_files/"
-
-			sleep 0.5
-			
+			flag=$seed"_"$alg"_"$nobs"_"$ti"_fnr_"$fn_rate"_af_"$adp_frac"_tau_"$tau
+			echo $flag
+			python3 $script "--adp_th" $ath "--fn_rate" $fn_rate "--tau" $tau "-m" $seed  "-n" $nobs "-b" $beta "-i" $ti "--adp_frac" $adp_frac 
 		done
 	done
 done
